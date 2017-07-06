92e7c51386c566235062db06e875939feaa39603
ChaosXWL <xuweiliang89@163.com>
2017-07-07 00:03:23 +0800

    Create database

---
+++ app/src/main/java/com/mystudy/android/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.mystudy.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by zhu on 2017/7/6.
+ */
+
+public class HttpUtil {
+    public static void sendRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
